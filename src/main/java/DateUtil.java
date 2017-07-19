02e2e52ed5c9efd016aa27cf0c2f68f031f45410
daiwei <18428385839@163.com>
2017-07-19 18:37:17 +0800

    增加日期函数

--- src/main/java/DateUtil.java
+++ src/main/java/DateUtil.java
@@ -3,13 +3,15 @@ import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
 * @Description 时间转换工具
-* @auther liyi7
-* @Date 2017年4月27日 下午5:14:19
-* @Copyright(c) gome inc Gome Co.,LTD
+* @auther diawei
+* @Date 2017年6月27日 下午5:14:19
  */
 public class DateUtil {
 	
@@ -80,5 +82,134 @@ public class DateUtil {
 		}
 		return date;
 	}
+
+	public static void main(String[] args) throws Exception {
+
+		//测试getDatesBetweenTwoDate
+		String start = "2014-01-03";
+		String end = "2014-03-05";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//		Date dBegin = sdf.parse(start);
+//		Date dEnd = sdf.parse(end);
+//		List<Date> listDate = getDatesBetweenTwoDate(dBegin, dEnd);
+		List<Date> listDate = getDatesBetweenTwoDate(start, end);
+		for(int i=0;i<listDate.size();i++){
+			System.out.println(sdf.format(listDate.get(i)));
+		}
+
+
+
+
+	}
+
+	/**
+	 * 根据开始时间和结束时间返回时间段内的时间集合
+	 *
+	 * @param beginDate
+	 * @param endDate
+	 * @return List
+	 */
+	public static List<Date> getDatesBetweenTwoDate(Date beginDate, Date endDate) {
+		List<Date> lDate = new ArrayList<Date>();
+		lDate.add(beginDate);// 把开始时间加入集合
+		Calendar cal = Calendar.getInstance();
+		// 使用给定的 Date 设置此 Calendar 的时间
+		cal.setTime(beginDate);
+		boolean bContinue = true;
+		while (bContinue) {
+			// 根据日历的规则，为给定的日历字段添加或减去指定的时间量
+			cal.add(Calendar.DAY_OF_MONTH, 1);
+			// 测试此日期是否在指定日期之后
+			if (endDate.after(cal.getTime())) {
+				lDate.add(cal.getTime());
+			} else {
+				break;
+			}
+		}
+		lDate.add(endDate);// 把结束时间加入集合
+		return lDate;
+	}
+
+	/**
+	 * 根据开始时间和结束时间返回时间段内的时间集合
+	 * @param startDate
+	 * @param endDate
+	 * @return
+	 * @throws ParseException
+	 */
+	public static List<Date> getDatesBetweenTwoDate(String startDate,String endDate) throws ParseException {
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		Date sDate =sdf.parse(startDate);
+		Date eDate =sdf.parse(endDate);
+		return getDatesBetweenTwoDate(sDate,eDate);
+	}
+
+
+
+
+	/**
+	 * 获取指定日期所在月份的天数
+	 *
+	 * @paramdate
+	 *            指定日期，yyyy-MM-dd
+	 * @return 天数
+	 */
+	public static final int getDaysOfMonth(int year, int month) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, year);
+		cal.set(Calendar.MONTH, month - 1);//Java月份才0开始算
+		return cal.getActualMaximum(Calendar.DATE);
+	}
+
+	/**
+	 * 获取当前年的天数
+	 * @param year
+	 * @return
+	 */
+	public static final int getDaysOfYear(int year) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, year);
+		return cal.getActualMaximum(Calendar.DAY_OF_YEAR);
+	}
+
+	/**
+	 * 计算两个日期之间相差的天数
+	 * @param smdate 较小的时间
+	 * @param bdate  较大的时间
+	 * @return 相差天数
+	 * @throws ParseException
+	 */
+	public static int daysBetween(Date smdate,Date bdate) throws ParseException
+	{
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		smdate=sdf.parse(sdf.format(smdate));
+		bdate=sdf.parse(sdf.format(bdate));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(smdate);
+		long time1 = cal.getTimeInMillis();
+		cal.setTime(bdate);
+		long time2 = cal.getTimeInMillis();
+		long between_days=(time2-time1)/(1000*3600*24);
+
+		return Integer.parseInt(String.valueOf(between_days));
+	}
+
+	/**
+	 * 计算两个日期之间相差的天数
+	 * @param startDate 较小的时间
+	 * @param endDate  较大的时间
+	 * @return 相差天数
+	 * @throws ParseException
+	 */
+	public static int daysBetween(String startDate,String endDate) throws ParseException
+	{
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		Date sDate =sdf.parse(startDate);
+		Date eDate =sdf.parse(endDate);
+		return daysBetween(sDate, eDate);
+	}
+
+
+
 }
 
