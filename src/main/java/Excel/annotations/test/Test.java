7097db28f9cc5ae801dde94657dcfdc2048b4f4d
daiwei <18428385839@163.com>
2017-08-10 19:47:07 +0800

    ava解析大数据量的excel

--- src/main/java/Excel/annotations/test/Test.java
+++ src/main/java/Excel/annotations/test/Test.java
@@ -2,6 +2,7 @@ package Excel.annotations.test;
 
 import Excel.annotations.ImportExcel;
 import Excel.entity.CouponUser;
+import Excel.entity.PrototypeManage;
 import Validation.PhoneValidationUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
@@ -16,6 +17,26 @@ import java.util.List;
  * Created by daiwei on 2017/7/27.
  */
 public class Test {
+//    /**
+//     * excel导入
+//     * @param path
+//     * @return
+//     * @throws IOException
+//     * @throws InvalidFormatException
+//     * @throws InvocationTargetException
+//     * @throws NoSuchMethodException
+//     * @throws InstantiationException
+//     * @throws IllegalAccessException
+//     */
+//    public static List<CouponUser> read(String path) throws IOException, InvalidFormatException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+//        List<CouponUser> couponUserList = new ArrayList<CouponUser>();
+//        InputStream fileInputStream = new FileInputStream(path);
+//        ImportExcel<CouponUser> couponUserImportExcel = new ImportExcel<CouponUser>(path, fileInputStream, 0, 0, CouponUser.class);
+//        couponUserList = couponUserImportExcel.getDataList(CouponUser.class);
+//        return couponUserList;
+//    }
+
+
     /**
      * excel导入
      * @param path
@@ -27,13 +48,30 @@ public class Test {
      * @throws InstantiationException
      * @throws IllegalAccessException
      */
-    public static List<CouponUser> read(String path) throws IOException, InvalidFormatException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        List<CouponUser> couponUserList = new ArrayList<CouponUser>();
+    public static List<PrototypeManage> read(String path) throws IOException, InvalidFormatException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        List<PrototypeManage> couponUserList = new ArrayList<PrototypeManage>();
         InputStream fileInputStream = new FileInputStream(path);
-        ImportExcel<CouponUser> couponUserImportExcel = new ImportExcel<CouponUser>(path, fileInputStream, 0, 0, CouponUser.class);
-        couponUserList = couponUserImportExcel.getDataList(CouponUser.class);
+        ImportExcel<PrototypeManage> couponUserImportExcel = new ImportExcel<PrototypeManage>(path, fileInputStream, 0, 0, PrototypeManage.class);
+        couponUserList = couponUserImportExcel.getDataList(PrototypeManage.class);
         return couponUserList;
     }
+
+//    /**
+//     * Excel导入测试
+//     * @param args
+//     */
+//    public static void main(String[] args) {
+//        try {
+//            //      List<CouponUser> couponUserList = ImportExcelUtils.read("d:/poi_test.xlsx");
+//            List<CouponUser> couponUserList = Test.read("d:/导入模板新.xlsx");
+//            for (int i = 0; i < couponUserList.size(); i++) {
+//                couponUserList.get(i).setPhone(PhoneValidationUtils.convertPhone(couponUserList.get(i).getPhone()));
+//                System.out.println(couponUserList.get(i).toString());
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     /**
      * Excel导入测试
@@ -42,9 +80,9 @@ public class Test {
     public static void main(String[] args) {
         try {
             //      List<CouponUser> couponUserList = ImportExcelUtils.read("d:/poi_test.xlsx");
-            List<CouponUser> couponUserList = Test.read("d:/导入模板新.xlsx");
+            List<PrototypeManage> couponUserList = Test.read("d:/竞争力模型数据-样机管理-201731.xlsx");
             for (int i = 0; i < couponUserList.size(); i++) {
-                couponUserList.get(i).setPhone(PhoneValidationUtils.convertPhone(couponUserList.get(i).getPhone()));
+             //   couponUserList.get(i).setPhone(PhoneValidationUtils.convertPhone(couponUserList.get(i).getPhone()));
                 System.out.println(couponUserList.get(i).toString());
             }
         } catch (Exception e) {
---
+++ src/main/java/Excel/gaosheng/BigExcelReaderTest.java
@@ -0,0 +1,28 @@
+package Excel.gaosheng;
+import Excel.gaosheng.BigExcelReader;
+
+import java.util.Arrays;
+
+/**
+ * java解析大数据量的excel
+ * @seehttp://blog.csdn.net/zp19890228/article/details/41822617
+ * @seehttp://blog.csdn.net/luka2008/article/details/8203915
+ */
+public class BigExcelReaderTest {
+    public static void main(String[] args) throws Exception{
+        String filepath = "d:/竞争力模型数据-样机管理-201731.xlsx";
+        BigExcelReader reader = new BigExcelReader(filepath) {
+            @Override
+            protected void outputRow(String[] datas, int[] rowTypes, int rowIndex) {
+                // 此处输出每一行的数据
+                System.out.println(Arrays.toString(datas));
+//                for (int i = 0; i <datas.length ; i++) {
+//                    String s=datas[i];
+//                }
+            }
+        };
+        // 执行解析
+        reader.parse();
+
+    }
+}
