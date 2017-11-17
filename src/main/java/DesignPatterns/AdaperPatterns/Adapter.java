66aedcc54f2090a46af7aab36bdc23c24c55f104
daiwei <18428385839@163.com>
2017-11-17 13:51:36 +0800

    迁移参数验证框架

--- src/main/java/DesignPatterns/AdaperPatterns/Adapter.java
+++ src/main/java/DesignPatterns/AdaperPatterns/Adapter.java
@@ -16,6 +16,7 @@ public class Adapter extends Adaptee implements Target {
      * 源类Adaptee有方法sampleOperation1
      * 因此适配器类直接委派即可
      */
+    @Override
     public void sampleOperation1(){
         this.adaptee.sampleOperation1();
     }
