2aa7797c66afab74a3b7d9e37cd9022b529d2aae
daiwei <18428385839@163.com>
2017-12-28 16:10:23 +0800

    观察者模式

---
+++ src/main/java/DesignPatterns/ObserverPatterns/jdk/Watched.java
@@ -0,0 +1,24 @@
+package DesignPatterns.ObserverPatterns.jdk;
+
+import java.util.Observable;
+
+/**
+ * 被观察者
+ * Created by daiwei on 2017/12/28.
+ */
+public class Watched extends Observable{
+    private String data = "";
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+
+        if(!this.data.equals(data)){
+            this.data = data;
+            setChanged();
+        }
+        notifyObservers();
+    }
+
+}
