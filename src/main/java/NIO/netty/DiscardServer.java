70af582bcf43170ca850a42542b2647b9131d1ba
daiwei <18428385839@163.com>
2018-02-09 10:39:46 +0800

    添加注释

--- src/main/java/NIO/netty/DiscardServer.java
+++ src/main/java/NIO/netty/DiscardServer.java
@@ -26,11 +26,19 @@ public class DiscardServer {
 
         /***
          * NioEventLoopGroup 是用来处理I/O操作的多线程事件循环器，
-         * Netty提供了许多不同的EventLoopGroup的实现用来处理不同传输协议。 在这个例子中我们实现了一个服务端的应用，
-         * 因此会有2个NioEventLoopGroup会被使用。 第一个经常被叫做‘boss’，用来接收进来的连接。
+         * <P>
+         *     Netty提供了许多不同的EventLoopGroup的实现用来处理不同传输协议。 在这个例子中我们实现了一个服务端的应用，
+         * 因此会有2个NioEventLoopGroup会被使用。
+         *
+         * 第一个经常被叫做‘boss’，用来接收进来的连接。
          * 第二个经常被叫做‘worker’，用来处理已经被接收的连接， 一旦‘boss’接收到连接，就会把连接信息注册到‘worker’上。
+         *
+         *
          * 如何知道多少个线程已经被使用，如何映射到已经创建的Channels上都需要依赖于EventLoopGroup的实现，
          * 并且可以通过构造函数来配置他们的关系。
+         *
+         * </P>
+         *
          */
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -51,8 +59,8 @@ public class DiscardServer {
              */
             b = b.channel(NioServerSocketChannel.class);
             /***
-             * 这里的事件处理类经常会被用来处理一个最近的已经接收的Channel。 ChannelInitializer是一个特殊的处理类，
-             * 他的目的是帮助使用者配置一个新的Channel。
+             * 这里的事件处理类经常会被用来处理一个最近的已经接收的Channel。
+             * ChannelInitializer是一个特殊的处理类，他的目的是帮助使用者配置一个新的Channel。
              * 也许你想通过增加一些处理类比如NettyServerHandler来配置一个新的Channel
              * 或者其对应的ChannelPipeline来实现你的网络程序。 当你的程序变的复杂时，可能你会增加更多的处理类到pipline上，
              * 然后提取这些匿名类到最顶层的类上。
