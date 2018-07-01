cc74d55f70265b130a4ac07f73ffe33c71b612e5
daiwei <18428385839@163.com>
2018-07-01 16:01:22 +0800

    移位运算

--- src/test/time/CalendarDemoTest.java
+++ src/test/time/CalendarDemoTest.java
@@ -12,6 +12,42 @@ public class CalendarDemoTest {
     @Test
     public void setCalendar() throws Exception {
         CalendarDemo.setCalendar();
+        String url="http://bjgc3-gos.gomecloud.com/ax-upload/storage/emulated/0/Android/data/com.gome.axlogistics/tiny/tiny-975-2018-06-15-14-22-54.jpg";
+        System.out.println(url.length());
+    }
+
+
+    /**
+     * 移位运算测试
+     * @throws Exception
+     */
+    @Test
+    public void shiftTest() throws Exception {
+        int number = 10;
+        //原始数二进制
+        printInfo(number);
+        number = number << 1;
+        //左移一位
+        printInfo(number);
+        number = number >> 1;
+        //右移一位
+        printInfo(number);
+
+        number = number<< 22;
+        System.out.println(number);
+        number = number>> 22;
+        System.out.println(number);
+
+    }
+
+
+    /**
+     * 输出一个int的二进制数
+     *@sees https://www.cnblogs.com/hongten/p/hongten_java_yiweiyunsuangfu.html
+     * @param num
+     */
+    private static void printInfo(int num){
+        System.out.println(Integer.toBinaryString(num));
     }
 
 }
