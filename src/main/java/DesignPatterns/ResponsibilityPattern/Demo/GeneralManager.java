f19172f83b8700599c5784fafefb9234f129f071
daiwei <18428385839@163.com>
2018-08-21 16:10:36 +0800

    责任链模式

--- src/main/java/DesignPatterns/ResponsibilityPattern/Demo/GeneralManager.java
+++ src/main/java/DesignPatterns/ResponsibilityPattern/Demo/GeneralManager.java
@@ -1,6 +1,7 @@
 package DesignPatterns.ResponsibilityPattern.Demo;
 
 /**
+ * 总经理
  * Created by daiwei on 2017/3/15.
  */
 public class GeneralManager extends Handler{
--- src/main/java/DesignPatterns/ResponsibilityPattern/README.md
+++ src/main/java/DesignPatterns/ResponsibilityPattern/README.md
@@ -1,2 +1,6 @@
 责任链模式
 
+
+简单地说目的就是找到真正的处理者对象
+
+@see https://www.cnblogs.com/java-my-life/archive/2012/05/28/2516865.html
