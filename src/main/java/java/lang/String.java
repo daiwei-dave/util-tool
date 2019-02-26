c2570ea5d5552742d0fd3b4b9fcdf89d824d5a16
daiwei <18428385839@163.com>
2019-02-26 17:08:11 +0800

    测试

---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,17 @@
+package java.lang;
+
+/**
+ * @author daiwei
+ * @date 2019/2/11 15:42
+ */
+public class String {
+    public static void main(String[] args) {
+//        String string=new String();
+//        string.say();
+        say();
+    }
+
+    public static void say(){
+        System.out.println("hahah");
+    }
+}
---
+++ src/main/java/multithreading/com/multithread/ThreadLocal/ThreadLocalWithOutSetTest.java
@@ -0,0 +1,50 @@
+package multithreading.com.multithread.ThreadLocal;
+
+/**
+ * Created by daiwei on 2018/1/22.
+ * @sees https://www.cnblogs.com/dolphin0520/p/3920407.html
+ */
+public class ThreadLocalWithOutSetTest {
+    ThreadLocal<Long> longLocal = new ThreadLocal<Long>();
+    ThreadLocal<String> stringLocal = new ThreadLocal<String>();
+
+
+    public void set() {
+        longLocal.set(Thread.currentThread().getId());
+        stringLocal.set(Thread.currentThread().getName());
+    }
+
+    public long getLong() {
+        return longLocal.get();
+    }
+
+    public String getString() {
+        return stringLocal.get();
+    }
+
+    public static void main(String[] args) {
+        final ThreadLocalWithOutSetTest threadLocalTest = new ThreadLocalWithOutSetTest();
+      //  threadLocalTest.set();
+        System.out.println(threadLocalTest.getLong());
+        System.out.println(threadLocalTest.getString());
+
+
+        Thread thread1 = new Thread(){
+            public void run() {
+                threadLocalTest.set();
+                System.out.println(threadLocalTest.getLong());
+                System.out.println(threadLocalTest.getString());
+            };
+        };
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(threadLocalTest.getLong());
+        System.out.println(threadLocalTest.getString());
+
+
+    }
+}
