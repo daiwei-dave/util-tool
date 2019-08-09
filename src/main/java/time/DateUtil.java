6de4c1adc6a3ad264c53af5eaa3586078192fbd0
daiwei <18428385839@163.com>
2019-08-09 09:42:39 +0800

    日期函数

--- src/main/java/time/DateUtil.java
+++ src/main/java/time/DateUtil.java
@@ -352,5 +352,40 @@ public class DateUtil {
 
 
 
+	/**
+	 * 获取指定日期的开始时间
+	 *
+	 * @param date
+	 * @return
+	 */
+	public static Date getCurrentDateStartTime(Date date) {
+		Calendar now = Calendar.getInstance();
+		now.setTime(date);
+		now.set(Calendar.HOUR_OF_DAY, 0);
+		now.set(Calendar.MINUTE, 0);
+		now.set(Calendar.SECOND, 0);
+		now.set(Calendar.MILLISECOND, 0);
+		return now.getTime();
+	}
+
+	/**
+	 * 获取指定日期的结束时间
+	 *
+	 * @param date
+	 * @return
+	 */
+	public static Date getCurrentDateEndTime(Date date) {
+		Calendar now = Calendar.getInstance();
+		now.setTime(date);
+		now.set(Calendar.HOUR_OF_DAY, 23);
+		now.set(Calendar.MINUTE, 59);
+		now.set(Calendar.SECOND, 59);
+		now.set(Calendar.MILLISECOND, 999);
+		return now.getTime();
+	}
+
+
+
+
 }
 
